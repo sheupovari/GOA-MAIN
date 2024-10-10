d3d909007f7524ee6d363b6b3d7d33e92bbfaa36
sheupovari <143882690+sheupovari@users.noreply.github.com>
2024-10-10 17:13:21 +0400

    Add files via upload

---
+++ day 10/homework.py
@@ -0,0 +1,4 @@
+#i will show you how you can print your name and last name 50 times
+for i in range(50):
+    print("saba wiqarishvili")
+    
---
+++ day 11/homework.py
@@ -0,0 +1,5 @@
+x = [42, 8, 7, 1, 0, 124, 8897, 555, 3, 67, 99]
+
+sum_of_numbers = sum(x)
+
+print("Sum of numbers is:", sum_of_numbers)
